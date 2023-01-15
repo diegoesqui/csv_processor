f748b771ba4bfdb5a4906e80955d170249c1edd7
Diego Esquinazi <diegoesqui@macbook-pro-diego.home>
2023-01-15 18:58:17 +0100

    changes on main code

--- run.py
+++ run.py
@@ -24,6 +24,10 @@ def clean(df):
     df[['amount', 'accountBalance']] = df[['amount', 'accountBalance']].astype('float')
     df['dateVal'] = pd.to_datetime(df['dateVal'], format="%d/%m/%Y")
     df.drop(columns=['comment', 'pointer', 'dateOp'], inplace=True)
+
+    df['year'] = df['dateVal'].dt.year
+    df['month'] = df['dateVal'].dt.month
+    df['year_month'] =  df['dateVal'].dt.strftime("%Y-%m")
     return df
 
 
@@ -64,6 +68,12 @@ def set_replacements(df, replacements):
     return df
 
 
+def compute_totals(df):
+    df = df.groupby(['year_month', 'category'], as_index=False).agg({'amount': 'sum'})
+    df = df.pivot(index='category', columns='year_month', values='amount').fillna(0)
+    return df
+
+
 def generate_output(df, output_path, add_timestamp=False):
     # create the output folder if it doesn't exist
     if not os.path.exists(output_path):
@@ -75,13 +85,28 @@ def generate_output(df, output_path, add_timestamp=False):
         output_name += current_date
 
     # export the data frame to a csv file in the output folder
-    df.to_csv(os.path.join(output_path, output_name + ".csv"), index=False, sep=',')
+    # df.to_csv(os.path.join(output_path, output_name + ".csv"), index=False, sep=',')
 
-    # export the data frame to a xlsx file in the output folder
-    df.to_excel(os.path.join(output_path, output_name + ".xlsx"), index=False)
+
+    # Separete Depenses from Revenues
+    df_dep = compute_totals(df[df.amount < 0].copy())
+    df_rev = compute_totals(df[df.amount > 0].copy())
+
+    output_url = os.path.join(output_path, output_name + ".xlsx")
+    with pd.ExcelWriter(output_url) as writer:  
+        df.to_excel(writer, sheet_name='all mouvements', index=False)
+        df_dep.to_excel(writer, sheet_name='Depenses')
+        df_rev.to_excel(writer, sheet_name='Revenues')
+
+        # for sheet in ['all mouvements', 'Depenses', 'Revenues']:
+        #     worksheet = writer.sheets[sheet]
+        #     for column in worksheet.column_dimensions:
+        #         worksheet.column_dimensions[column].auto_size = True
+
     return
+
 
 
 if __name__ == "__main__":
     main()
-    input("Press Enter to continue...")
+    # input("Press Enter to continue...")
